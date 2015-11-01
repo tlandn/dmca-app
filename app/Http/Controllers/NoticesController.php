3fa9b375e79d3c8d6aea2ad70adc244f85fe5ed2
Tuan Nguyen <anhtuantt@gmail.com>
2015-11-01 20:40:21 +0700

    Xong DMCA-8-Confirming-the-DMCA-Notice

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PrepareNoticeRequest;
 use App\Provider;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -33,7 +34,38 @@ class NoticesController extends Controller
         return view('notices.create', compact('providers'));
     }
 
-    public function confirm(PrepareNoticeRequest $request) {
-        return $request->all();
+
+    public function confirm(PrepareNoticeRequest $request, Guard $auth)
+    {
+        $template = $this->compileDmcaTemplate($data = $request->all(), $auth);
+        session()->flash('dmca', $data);
+
+        return view('notices.confirm', compact('template'));
     }
+
+
+    /**
+     *  Store a new DMCA notice.
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $data = session()->get('dmca');
+        return $data;
+    }
+
+
+    public function compileDmcaTemplate($data, Guard $auth)
+    {
+        $data = $data + [
+                'name'  => $auth->user()->name,
+                'email' => $auth->user()->email,
+            ];
+
+        return view()->file(app_path('Http/Templates/dmca.blade.php'), $data);
+    }
+
 }
